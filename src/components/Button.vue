ff5ea25c3508307db1857a526d4d83b3901efe22
David Mtz G <dmartinezg1204@alumno.ipn.mx>
2023-04-05 17:43:23 -0600

    Fin, listo para deploy

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+    const props = defineProps(['operador', 'fn']);
+
+</script>
+
+<template>
+
+    <button
+        type="button"
+        class="h-10 w-10 flex items-center justify-center rounded-full font-bold bg-lime-500 hover:outline-none hover:ring-2 
+            hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl"
+        @click="fn"
+        
+    >
+        {{ operador }}
+    </button>
+
+</template>
